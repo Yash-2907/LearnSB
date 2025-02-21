4d41679a5bf0f828e421413e475bf27b0bf2c60c
Yash-2907 <guptayp_1@rknec.edu>
2025-02-22 02:17:34 +0530

    jstl

--- myfirstwebapp/src/main/java/com/example/in28mins/myfirstwebapp/todo/todoService.java
+++ myfirstwebapp/src/main/java/com/example/in28mins/myfirstwebapp/todo/todoService.java
@@ -10,10 +10,10 @@ import org.springframework.stereotype.Component;
 public class todoService {
     private static List<Todo> tasksList=new ArrayList<Todo>();
     static {
-    	tasksList.add(new Todo('0',"yash","learn this course",LocalDate.now(),true));
-    	tasksList.add(new Todo('1',"yash","do dsa",LocalDate.now().plusYears(1),false));
-    	tasksList.add(new Todo('2',"yash","get placed",LocalDate.now().plusYears(2),false));
-    	tasksList.add(new Todo('3',"yash","learn more",LocalDate.now().plusYears(3),false));
+    	tasksList.add(new Todo(0,"yash","learn this course",LocalDate.now(),true));
+    	tasksList.add(new Todo(1,"yash","do dsa",LocalDate.now().plusYears(1),false));
+    	tasksList.add(new Todo(2,"yash","get placed",LocalDate.now().plusYears(2),false));
+    	tasksList.add(new Todo(3,"yash","learn more",LocalDate.now().plusYears(3),false));
     }
     
     public List<Todo> getList()
--- myfirstwebapp/src/main/resources/META-INF/resources/WEB-INF/jsp/todoJsp.jsp
+++ myfirstwebapp/src/main/resources/META-INF/resources/WEB-INF/jsp/todoJsp.jsp
@@ -1,9 +1,34 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
 <html>
 <head>
 <title>This is the todo page</title>
 </head>
 <body>
    Hi ${name}
-   Your pending tasks are : ${yourTodo}
+   <br>
+   <h2>Your pending tasks are :</h2> 
+   <table>
+     <thead>
+        <tr>
+          <th>id</th>
+          <th>username</th>
+          <th>Description</th>
+          <th>TargetDate</th>
+          <th>isDone</th>
+        </tr>
+     </thead>
+     <tbody>
+         <c:forEach items="${yourTodo}" var="todos">
+				<tr>
+					<th>${todos.id}</th>
+					<th>${todos.username}</th>
+					<th>${todos.description}</th>
+					<th>${todos.targetDate}</th>
+					<th>${todos.isdone}</th>
+				</tr>
+		 </c:forEach>
+     </tbody>
+   </table>
 </body>
 </html>
